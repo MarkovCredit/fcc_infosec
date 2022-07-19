7eb9dbab87e044c82aa4e24df36f457f66734169
Mark Levine <mark-levine-1@outlook.com>
2022-07-18 20:38:32 -0700

    trying this again again

--- myApp.js
+++ myApp.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const helmet = require("helmet");
-app.use(helmet.hidePoweredBy({setTo: 'PHP 4.4.1'}))
 const app = express();
-
+app.use(helmet.hidePoweredBy({setTo: 'PHP 4.4.1'}))
 
 
 
