b6cac4d5b7034e765f02c1a3fb552cfef5f99c19
Mark Levine <mark-levine-1@outlook.com>
2022-07-19 19:28:19 -0700

    server.js had an extra entry for helmet

--- server.js
+++ server.js
@@ -15,10 +15,7 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers":
       "Origin, X-Requested-With, content-type, Accept",
-  });
-  helmet({
-
-  })
+  }); 
   app.disable("x-powered-by");
   next();
 });
